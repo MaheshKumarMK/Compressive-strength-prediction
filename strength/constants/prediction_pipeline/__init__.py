7dcc5db49c0b0108532414a88ef43a205d1db13f
MaheshKumarMK <maheshmkvb92@gmail.com>
2023-01-26 17:05:18 +0530

    created prediction constant file

---
+++ strength/constants/prediction_pipeline/__init__.py
@@ -0,0 +1,24 @@
+SCHEMA_PREDICTION='schema_prediction.json'
+
+PREDICTION_PATH = 'Prediction_Raw_Files_Validated/'
+
+PREDICTION_GOOD_RAW_VALIDATED_DIR = os.path.join(PREDICTION_PATH, "Good_Raw/")
+
+PREDICTION_BAD_RAW_VALIDATED_DIR = os.path.join(PREDICTION_PATH, "Bad_Raw/")
+
+
+PREDICTION_ARCHIVE_BAD="PredictionArchivedBadData"
+
+PREDICTION_BATCH_FILES="Prediction_Batch_files/"
+
+PREDICTION_OUTPUT_FILE='Prediction_Output_File/Predictions.csv'
+
+DATABASE = 'Prediction_Database/'
+
+PREDICTION_FILE_DB = 'Prediction_FileFromDB/'
+
+PRED_INPUT_FILE='Prediction_FileFromDB/InputFile.csv'
+
+
+
+
