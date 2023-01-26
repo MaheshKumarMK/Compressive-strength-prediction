540cac5ad0e5f258f041389b426e1d37e722bc27
MaheshKumarMK <maheshmkvb92@gmail.com>
2023-01-26 17:03:54 +0530

    created prediction log directory and file path

--- strength/constants/log_files/__init__.py
+++ strength/constants/log_files/__init__.py
@@ -1,23 +1,47 @@
 import os
 
-GENERAL_LOG=open(os.path.join("training_artifact", "Training_Logs/GeneralLog.txt","a+"))
+GENERAL_LOG=os.path.join("Training_Logs","GeneralLog.txt")
 
-COLUMN_VALIDATION_LOG = open(os.path.join("training_artifact","Training_Logs/columnValidationLog.txt", 'a+'))
+COLUMN_VALIDATION_LOG = (os.path.join("Training_Logs","columnValidationLog.txt"))
 
-MISSING_VALUES_LOG = open(os.path.join("training_artifact","Training_Logs/missingValuesInColumn.txt", 'a+'))
+MISSING_VALUES_LOG = (os.path.join("Training_Logs","missingValuesInColumn.txt"))
 
-SCHEMA_VALIDATION_LOG = open(os.path.join("training_artifact","Training_Logs/valuesfromSchemaValidationLog.txt", 'a+'))
+SCHEMA_VALIDATION_LOG = (os.path.join("Training_Logs","valuesfromSchemaValidationLog.txt"))
 
-MAIN_TRAINING_LOG = open(os.path.join("training_artifact", "Training_Logs/Training_Main_Log.txt"),"a+")
+MAIN_TRAINING_LOG = (os.path.join("Training_Logs","Training_Main_Log.txt"))
 
-NAME_VALIDATION_LOG = open(os.path.join("training_artifact","Training_Logs/nameValidationLog.txt", 'a+'))
+NAME_VALIDATION_LOG = (os.path.join("Training_Logs","nameValidationLog.txt"))
 
-DATABASE_CONN_LOG = open(os.path.join("training_artifact", "Training_Logs/DataBaseConnectionLog.txt", 'a+'))
+DATABASE_CONN_LOG = (os.path.join("Training_Logs","DataBaseConnectionLog.txt"))
 
-DB_TABLE_LOG = open(os.path.join("training_artifact","Training_Logs/DbTableCreateLog.txt", 'a+'))
+DB_TABLE_LOG = (os.path.join("Training_Logs","DbTableCreateLog.txt"))
 
-DB_INSERT_LOG = open(os.path.join("training_artifact","Training_Logs/DbInsertLog.txt", 'a+'))
+DB_INSERT_LOG = (os.path.join("Training_Logs","DbInsertLog.txt"))
 
-MODEL_TRAINING_LOG = open(os.path.join("training_artifact","Training_Logs/ModelTrainingLog.txt", 'a+'))
+MODEL_TRAINING_LOG = (os.path.join("Training_Logs","ModelTrainingLog.txt"))
+
+
+
+# Prediction logs
+
+PREDICTION_SCHEMA_VALIDATION_LOG = os.path.join("Prediction_Logs,valuesfromSchemaValidationLog.txt")
+
+PREDICTION_GENERAL_LOG = "Prediction_Logs/GeneralLog.txt"
+
+PREDICTION_NAME_VALIDATION_LOG="Prediction_Logs/nameValidationLog.txt"
+
+PREDICTION_COLUMN_VALIDATION_LOG="Prediction_Logs/columnValidationLog.txt"
+
+PREDICTION_MISSING_VALUE = "Prediction_Logs/missingValuesInColumn.txt",
+
+PREDICTION_DATABASE_CONN_LOG="Prediction_Logs/DataBaseConnectionLog.txt"
+
+PREDICTION_DB_TABLE_CREATION_LOG="Prediction_Logs/DbTableCreateLog.txt"
+
+PREDICTION_DB_INSERT_LOG = "Prediction_Logs/DbInsertLog.txt"
+
+PREDICTION_LOGS_EXPORT_CSV="Prediction_Logs/ExportToCsv.txt"
+
+
 
 
